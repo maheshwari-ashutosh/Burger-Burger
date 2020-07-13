23e655198aeae11f7cde62f8953ef9a8a2ac2fe2
Ashutosh Maheshwari <ashutosh.ismcse@gmail.com>
2020-07-13 22:38:45 +0530

    Updates

---
+++ precache-manifest.ac5599e9c87e529637a2bf7bfce07a16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5bdb5eef34f03adb758dd9b0acb40b9",
+    "url": "/Burger-Burger/index.html"
+  },
+  {
+    "revision": "b4722f7a23b7b8441aa7",
+    "url": "/Burger-Burger/static/css/main.2df1fdab.chunk.css"
+  },
+  {
+    "revision": "3e747d0a868c5c9d7fc6",
+    "url": "/Burger-Burger/static/js/2.f3bb2698.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Burger-Burger/static/js/2.f3bb2698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4722f7a23b7b8441aa7",
+    "url": "/Burger-Burger/static/js/main.024722fb.chunk.js"
+  },
+  {
+    "revision": "f3f9b01e5f14ed908ebf",
+    "url": "/Burger-Burger/static/js/runtime-main.e08189dd.js"
+  }
+]);
